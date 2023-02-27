e192128acbecbb23983e1fc951827dd6e42b3e53
aminesaadaoui <104393874+aminesaadaoui@users.noreply.github.com>
2023-02-27 11:02:26 +0100

    items and tools final

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance { get; private set; }
+
+    [Header("Inventory System")]
+    public GameObject inventoryPanel;
+    public InventorySlot[] toolSlots;
+    public InventorySlot[] itemSlots;
+
+    public Text itemNameText;
+    public Text itemDescriptionText;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        RenderInventory();
+    }
+
+   public void RenderInventory()
+    {
+        ItemData[] inventoryToolSlots = InventoryManager.Instance.tools;
+      
+        ItemData[] inventoryItemSlots = InventoryManager.Instance.items;
+
+
+        RenderInventoryPanel(inventoryToolSlots, toolSlots);
+
+        RenderInventoryPanel(inventoryItemSlots, itemSlots);
+    }
+
+    void RenderInventoryPanel(ItemData[] slots, InventorySlot[] uiSlots)
+    {
+        for (int i = 0; i < uiSlots.Length; i++)
+        {
+            uiSlots[i].Display(slots[i]);
+        }
+
+    }
+
+
+    public void ToogleInventoryPanel()
+    {
+        inventoryPanel.SetActive(!inventoryPanel.activeSelf);
+       
+        RenderInventory();
+    }
+
+    public void DisplayItemInfo(ItemData data)
+    {
+        if(data == null)
+        {
+            itemNameText.text = "";
+            itemDescriptionText.text = "";
+
+            return;
+        }
+
+        itemNameText.text = data.name;
+        itemDescriptionText.text = data.description;    
+    }
+}
